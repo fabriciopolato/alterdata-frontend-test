a66cd2e50749c3224d034190308e4b5a3a7c8638
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-29 18:48:08 -0300

    feat: add Modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import close from '../../assets/close.svg';
+
+import { Container, Content, CloseIcon } from './styles';
+
+interface IProps {
+  toggle: boolean;
+  handleToggle: () => void;
+  id: any;
+  closeIconLeft?: boolean;
+}
+
+const Modal: React.FC<IProps> = ({ toggle, handleToggle, id, children }) => {
+  return (
+    <Container toggle={toggle}>
+      <Content id={id}>
+        <CloseIcon src={close} alt="Fechar" onClick={handleToggle} />
+        {children}
+      </Content>
+    </Container>
+  );
+};
+
+export default Modal;
