57ca7e0e9da4c82e0ff8e3f8d156866e40346f47
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-29 18:47:46 -0300

    feat: add clickedTicket

---
+++ src/components/ClickedTicket/index.tsx
@@ -0,0 +1,24 @@
+import React, { HTMLAttributes } from 'react';
+import { Container, Content } from './styles';
+import { ITicket } from '../../interfaces/interfaces';
+
+interface PropsTickets extends HTMLAttributes<HTMLDivElement> {
+  ticket: ITicket;
+}
+
+const ClickedTicket: React.FC<PropsTickets> = ({ ticket, ...rest }) => {
+  const { subject, message, created_at, author, deleted_at } = ticket;
+  return (
+    <Container {...rest}>
+      <Content>
+        <h1>{subject}</h1>
+        <p>{message}</p>
+        <span>{created_at}</span>
+      </Content>
+      <span>{author}</span>
+      <span>{deleted_at}</span>
+    </Container>
+  );
+};
+
+export default ClickedTicket;
