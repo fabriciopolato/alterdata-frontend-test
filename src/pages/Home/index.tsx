11bd80e139507177251e71d74b4af515f1b4e1bc
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-07-29 18:49:18 -0300

    refactor: Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,68 @@
-import React from 'react';
-import { Ticket } from '../../components';
+import React, { useEffect, useState } from 'react';
+import { Ticket, Modal, ClickedTicket } from '../../components';
 import { Container, OpenTickets, ClosedTickets } from './styles';
+import { getAllTickets, getAllClosedTickets } from '../../services/api';
+import { ITicket } from '../../interfaces/interfaces';
 
 const Home: React.FC = () => {
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+  const [allOpenTickets, setAllOpenTickets] = useState<ITicket[]>([]);
+  const [allClosedTickets, setAllClosedTickets] = useState<ITicket[]>([]);
+  const [clickedTicket, setClickedTicket] = useState<ITicket>({} as ITicket);
+
+  useEffect(() => {
+    (async () => {
+      const openTickets = await getAllTickets();
+      const closedTickets = await getAllClosedTickets();
+      setAllOpenTickets(openTickets.data);
+      setAllClosedTickets(closedTickets.data);
+    })();
+  }, []);
+
+  const handleToggleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  const handleClickTicket = (clickedTicket: ITicket) => {
+    setClickedTicket(clickedTicket);
+  };
+
   return (
-    <Container>
-      <OpenTickets>{/* <Ticket ticket={} /> */}</OpenTickets>
-      <ClosedTickets>{/* <Ticket ticket={} /> */}</ClosedTickets>
-    </Container>
+    <>
+      <Modal toggle={toggleMenu} handleToggle={handleToggleMenu} id={clickedTicket?.id}>
+        <ClickedTicket ticket={clickedTicket} />
+      </Modal>
+      <Container>
+        <OpenTickets>
+          {allOpenTickets.map(ticket => {
+            return (
+              <Ticket
+                onClick={() => {
+                  handleToggleMenu();
+                  handleClickTicket(ticket);
+                }}
+                key={ticket.id}
+                ticket={ticket}
+              />
+            );
+          })}
+        </OpenTickets>
+        <ClosedTickets>
+          {allClosedTickets.map(ticket => {
+            return (
+              <Ticket
+                onClick={() => {
+                  handleToggleMenu();
+                  handleClickTicket(ticket);
+                }}
+                key={ticket.id}
+                ticket={ticket}
+              />
+            );
+          })}
+        </ClosedTickets>
+      </Container>
+    </>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-color: #ccc;
+  height: 100vh;
+`;
 export const OpenTickets = styled.div``;
 export const ClosedTickets = styled.div``;
