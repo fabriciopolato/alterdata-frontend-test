fe62077079cff908ab9f67f03a4ca4c52acae9ea
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-08-01 07:54:58 -0300

    feat: add logout functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,16 +3,28 @@ import { Button } from '../';
 import { Container, Content } from './styles';
 import { ReactComponent as HomeIcon } from '../../assets/home.svg';
 import { Context } from '../../context/context';
+import { useAuth } from '../../hooks/auth';
+import { useHistory } from 'react-router-dom';
 
 const Header: React.FC = () => {
   const { handleToggleModalNewTicket } = useContext(Context);
+  const history = useHistory();
+
+  const { signOut } = useAuth();
 
   return (
     <Container>
       <Content>
         <Button onClick={handleToggleModalNewTicket}>Criar Ticket</Button>
         <HomeIcon />
-        <Button isBlue isTransparent>
+        <Button
+          onClick={() => {
+            signOut();
+            history.push('/');
+          }}
+          isBlue
+          isTransparent
+        >
           Logout
         </Button>
       </Content>
